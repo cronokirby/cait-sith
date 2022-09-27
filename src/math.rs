415658b1d643a6c109f6ada9328d671836451e48
Lúcás C. Meier <lucas@cronokirby.com>
2022-09-27 22:37:41 +0200

    Add a method to extend a constant to a random polynomial

--- src/math.rs
+++ src/math.rs
@@ -16,7 +16,20 @@ impl Polynomial {
         let coefficients = (0..size)
             .map(|_| Scalar::generate_biased(&mut *rng))
             .collect();
-        Polynomial { coefficients }
+        Self { coefficients }
+    }
+
+    /// Extend a constant to a random polynomial of a certain size.
+    ///
+    /// This is useful if you want the polynomial to have a certain value, but
+    /// otherwise be random.
+    pub fn extend_random(rng: &mut impl CryptoRngCore, size: usize, constant: &Scalar) -> Self {
+        let mut coefficients = Vec::with_capacity(size);
+        coefficients.push(*constant);
+        for _ in 1..size {
+            coefficients.push(Scalar::generate_biased(&mut *rng));
+        }
+        Self { coefficients }
     }
 }
 
