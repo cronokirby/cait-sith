a049910b5cea8ee03f1b071f20cb5d7b2346f414
Lúcás C. Meier <lucas@cronokirby.com>
2022-09-27 23:03:08 +0200

    Implement scaling for polynomials

--- src/math.rs
+++ src/math.rs
@@ -1,11 +1,11 @@
-use auto_ops::impl_op_ex;
+use auto_ops::{impl_op_ex, impl_op_ex_commutative};
 use std::ops::Index;
 
 use k256::Scalar;
 use rand_core::CryptoRngCore;
 
 /// Represents a polynomial with coefficients in the scalar field of the curve.
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Polynomial {
     /// The coefficients of our polynomial, from 0..size-1.
     coefficients: Vec<Scalar>,
@@ -49,6 +49,18 @@ impl Polynomial {
         out.add_mut(other);
         out
     }
+
+    /// Scale this polynomial in place by a field element.
+    pub fn scale_mut(&mut self, scale: &Scalar) {
+        self.coefficients.iter_mut().for_each(|a| *a *= scale);
+    }
+
+    /// Return the result of scaling this polynomial by a field element.
+    pub fn scale(&self, scale: &Scalar) -> Self {
+        let mut out = self.clone();
+        out.scale_mut(scale);
+        out
+    }
 }
 
 impl Index<usize> for Polynomial {
@@ -61,6 +73,8 @@ impl Index<usize> for Polynomial {
 
 impl_op_ex!(+ |f: &Polynomial, g: &Polynomial| -> Polynomial { f.add(g) });
 impl_op_ex!(+= |f: &mut Polynomial, g: &Polynomial| { f.add_mut(g) });
+impl_op_ex_commutative!(*|f: &Polynomial, s: &Scalar| -> Polynomial { f.scale(s) });
+impl_op_ex!(*= |f: &mut Polynomial, s: &Scalar| { f.scale_mut(s) });
 
 #[cfg(test)]
 mod test {
@@ -79,6 +93,20 @@ mod test {
         };
         assert_eq!(&f + &g, h);
         f += &g;
+        assert_eq!(f, h);
+    }
+
+    #[test]
+    fn test_scaling() {
+        let s = Scalar::from(2u32);
+        let mut f = Polynomial {
+            coefficients: vec![Scalar::from(1u32), Scalar::from(2u32)],
+        };
+        let h = Polynomial {
+            coefficients: vec![Scalar::from(2u32), Scalar::from(4u32)],
+        };
+        assert_eq!(s * &f, h);
+        f *= s;
         assert_eq!(f, h);
     }
 }
