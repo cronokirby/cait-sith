8c1fba6b21866a878c2f95233db6f4b016253ab3
Lúcás C. Meier <lucas@cronokirby.com>
2022-10-01 23:18:42 +0200

    Make protocol types public

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -3,7 +3,7 @@ use std::error;
 
 /// Represents an error which can happen when running a protocol.
 #[derive(Debug)]
-enum ProtocolError {
+pub enum ProtocolError {
     /// Some generic error happened.
     Other(Box<dyn error::Error>),
 }
@@ -30,8 +30,8 @@ impl Into<ProtocolError> for Box<dyn error::Error> {
 /// struct holds. In our case, we use a `u32`, which is enough for billions of
 /// participants. That said, you won't actually be able to make the protocols
 /// work with billions of users.
-#[derive(Debug, Clone, Copy, PartialEq)]
-struct Participant(u32);
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Participant(u32);
 
 impl Into<Participant> for u32 {
     fn into(self) -> Participant {
@@ -49,7 +49,7 @@ impl Into<u32> for Participant {
 ///
 /// We choose to just represent messages as opaque vectors of bytes, with all
 /// the serialization logic handled internally.
-type MessageData = Vec<u8>;
+pub type MessageData = Vec<u8>;
 
 /// Represents an action by a participant in the protocol.
 ///
@@ -60,7 +60,7 @@ type MessageData = Vec<u8>;
 ///
 /// Eventually, the participant returns a value, ending the protocol.
 #[derive(Debug, Clone)]
-enum Action<T> {
+pub enum Action<T> {
     /// Don't do anything.
     Wait,
     /// Send a message to all other participants.
@@ -81,14 +81,15 @@ enum Action<T> {
 /// Basically, this represents a struct for the behavior of a single participant
 /// in a protocol. The idea is that the computation of that participant is driven
 /// mainly by receiving messages from other participants.
-trait Protocol {
+pub trait Protocol {
     type Output;
 
     /// Start the execution of the protocol, returning the action of this participant.
-    fn start() -> Action<Self::Output>;
+    fn start() -> Result<Action<Self::Output>, ProtocolError>;
 
     /// Advance this protocol with a message from some participant.
-    fn advance(from: Participant, data: MessageData) -> Action<Self::Output>;
+    fn advance(from: Participant, data: MessageData)
+        -> Result<Action<Self::Output>, ProtocolError>;
 }
 
-mod internal;
+pub(crate) mod internal;
