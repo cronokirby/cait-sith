80d293f50529f89ea63b8c84c21804b29186a378
Lúcás C. Meier <lucas@cronokirby.com>
2023-02-21 19:40:13 +0100

    Update dependencies

--- src/compat.rs
+++ src/compat.rs
@@ -1,14 +1,14 @@
 use digest::{Digest, FixedOutput};
 use ecdsa::{
-    elliptic_curve::{ops::Reduce, AffineXCoordinate, Curve, PrimeField},
+    elliptic_curve::{ops::Reduce, AffineXCoordinate, Curve},
     hazmat::DigestPrimitive,
 };
-use k256::{AffinePoint, Scalar, Secp256k1};
+use k256::{AffinePoint, FieldBytes, Scalar, Secp256k1};
 
 pub fn scalar_hash(msg: &[u8]) -> Scalar {
     let digest = <Secp256k1 as DigestPrimitive>::Digest::new_with_prefix(&msg);
-    let m_bytes = Secp256k1::prehash_to_field_bytes(&digest.finalize_fixed()).unwrap();
-    Option::<Scalar>::from(Scalar::from_repr(m_bytes)).unwrap()
+    let m_bytes: FieldBytes = digest.finalize_fixed();
+    <Scalar as Reduce<<Secp256k1 as Curve>::UInt>>::from_be_bytes_reduced(m_bytes)
 }
 
 pub fn x_coordinate(point: &AffinePoint) -> Scalar {
