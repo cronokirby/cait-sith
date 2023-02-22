bb10f4b2cda9cf791cbb0aff024ba254725f8948
Lúcás C. Meier <lucas@cronokirby.com>
2023-02-22 14:43:07 +0100

    Document triple module

--- src/triples/mod.rs
+++ src/triples/mod.rs
@@ -1,3 +1,30 @@
+//! This module contains the types and protocols related to triple generation.
+//! 
+//! The cait-sith signing protocol makes use of *committed* Beaver Triples.
+//! A triple is a value of the form `(a, b, c), (A, B, C)`, such that
+//! `c = a * b`, and `A = a * G`, `B = b * G`, `C = c * G`. This is a beaver
+//! triple along with commitments to its values in the form of group elements.
+//! 
+//! The signing protocols make use of a triple where the scalar values `(a, b, c)`
+//! are secret-shared, and the commitments are public. Each signature requires
+//! two triples. These triples can be generated in advance without knowledge
+//! of the secret key used to sign. It's important that the value of the underlying
+//! scalars in the triple is kept secret, otherwise the private key used to create
+//! a signature with that triple could be recovered.
+//! 
+//! There are two ways of generating these triples.
+//! 
+//! One way is to have
+//! a trusted third party generate them. This is supported by the [deal] function.
+//! 
+//! The other way is to run a protocol generating a secret shared triple without any party
+//! learning the secret values. This is better because no party learns the value of the
+//! triple, which needs to be kept secret. This method is supported by the [generate_triple]
+//! protocol.
+//! 
+//! This protocol requires a setup protocol to be one once beforehand.
+//! After this setup protocol has been run, an arbitarary number of triples can
+//! be generated.
 use k256::{AffinePoint, ProjectivePoint, Scalar};
 use rand_core::CryptoRngCore;
 use serde::Serialize;
